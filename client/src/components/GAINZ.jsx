89f73840c4c1a11e67c91f8a7af860b27e6ce40c
Wenny Xiong <wenny.xiong7@gmail.com>
2022-08-13 13:10:23 -0400

    added style folder and general styles file

--- client/src/components/GAINZ.jsx
+++ client/src/components/GAINZ.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import { LogoutButton } from './Userfront/Userfront';
+import GS from './styles/GeneralStyles.jsx';
 
 export default function GAINZ() {
   return (
-    <>
-      <div>GAINZ</div>
+    <GS.Background>
+      <div>
+        GAINZ
+      </div>
       <LogoutButton />
-    </>
+    </GS.Background>
   );
 }
---
+++ client/src/components/styles/GeneralStyles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const GS = {
+  Background: styled.div`
+    width: 100%;
+    /* height: 100%; */
+    max-width: 1680px;
+    z-index: 0;
+    background-color: #121212;
+    color: #ffffff;
+  `,
+};
+
+export default GS;
