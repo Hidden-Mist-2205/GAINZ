edcb8ccb93b13c2e2fb61eb6407f92eec487223f
Albert <albertthuynh94@gmail.com>
2022-08-15 16:59:48 -0400

    fixed server indexjs back to normal

--- server/index.js
+++ server/index.js
@@ -7,20 +7,20 @@ const controllers = require('../database/controllers');
 const app = express();
 
 // auth middleware for Userfront
-// function authenticateToken(req, res, next) {
-//   // eslint-disable-next-line dot-notation
-//   const authHeader = req.headers['authorization'];
-//   const token = authHeader && authHeader.split(' ')[1];
-//   if (token == null) return res.sendStatus(401);
+function authenticateToken(req, res, next) {
+  // eslint-disable-next-line dot-notation
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
 
-//   const USERFRONT_PUBLIC_KEY = atob(process.env.USERFRONT_PUBLIC_KEY_B64);
-//   // eslint-disable-next-line consistent-return
-//   jwt.verify(token, USERFRONT_PUBLIC_KEY, (err, auth) => {
-//     if (err) return res.sendStatus(403);
-//     req.auth = auth;
-//     next();
-//   });
-// }
+  const USERFRONT_PUBLIC_KEY = atob(process.env.USERFRONT_PUBLIC_KEY_B64);
+  // eslint-disable-next-line consistent-return
+  jwt.verify(token, USERFRONT_PUBLIC_KEY, (err, auth) => {
+    if (err) return res.sendStatus(403);
+    req.auth = auth;
+    next();
+  });
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
