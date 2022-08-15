2bf76b0b9f1b8cc6f48a8efa166ee7cd59e1c932
Max McKenna <mckennamax34@gmail.com>
2022-08-15 17:27:13 -0400

    Changed some sizing to improve appearance, added sample data to create mapping functionality

--- client/src/components/MyGainz/Workout.jsx
+++ client/src/components/MyGainz/Workout.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { completedWorkoutsState, pageState } from './workoutsAtom';
 import MG from '../styles/MyGainz_style/MG';
 import WorkoutPanel from './WorkoutPanel';
 
 export default function Workout() {
-  return (
-    <MG.WOBody>
-      <WorkoutPanel />
-      <WorkoutPanel />
-      <WorkoutPanel />
-      <WorkoutPanel />
-    </MG.WOBody>
+  const completedWorkouts = useRecoilValue(completedWorkoutsState);
+  const page = useRecoilValue(pageState);
+  const displayedWorkouts = completedWorkouts.slice(page.start, page.end);
+  return (displayedWorkouts.length > 0
+    ? (
+      <MG.WOBody>
+        {displayedWorkouts.map((workout) => <WorkoutPanel key={workout.ID} workout={workout} />)}
+      </MG.WOBody>
+    )
+    : (
+      <MG.WOBody>
+        <MG.NoWorkouts>
+          It looks like you have not worked out yet. Head to the
+          <MG.LibraryLink href="/UserLibrary"> &nbsp;Library&nbsp;</MG.LibraryLink>
+          and make some gainz!
+        </MG.NoWorkouts>
+      </MG.WOBody>
+    )
   );
 }
--- client/src/components/MyGainz/WorkoutPanel.jsx
+++ client/src/components/MyGainz/WorkoutPanel.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import MG from '../styles/MyGainz_style/MG';
 import GS from '../styles/GeneralStyles';
 import ExercisePanel from './ExercisePanel';
 
-export default function WorkoutPanel() {
-  const [favorite, setFavorite] = useState(false);
+export default function WorkoutPanel({ workout }) {
+  const [favorite, setFavorite] = useState(workout.favorited);
   const [showExercises, setShowExercises] = useState(false);
 
   const handleFavorite = (e) => {
     e.preventDefault();
+    // put request to update favorited value, or delete row from Users/Workouts table?
     setFavorite(!favorite);
   };
 
@@ -17,16 +19,27 @@ export default function WorkoutPanel() {
     setShowExercises(!showExercises);
   };
 
+  const navigate = useNavigate();
+  const routeChange = () => {
+    // change path to add workout ID
+    const path = '/StartWorkout';
+    navigate(path);
+  };
+
   return (
     <MG.WOPanel>
       <MG.WOItem>
         {favorite
           ? <MG.WOStar onClick={handleFavorite}>&#9734;</MG.WOStar>
           : <MG.WOStar onClick={handleFavorite}>&#9733;</MG.WOStar>}
-        <MG.WOName onClick={handleExercisePanel}>Workout Name</MG.WOName>
-        <MG.WOTimesCompleted>Completed x times</MG.WOTimesCompleted>
-        <MG.WOLastCompleted>Last completed on: MM/DD/YY 00:00 AM</MG.WOLastCompleted>
-        <GS.Button>START</GS.Button>
+        <MG.WOName onClick={handleExercisePanel}>{workout.Name}</MG.WOName>
+        <MG.WOTimesCompleted>
+          {workout.TimesCompleted === 1
+            ? `Completed ${workout.TimesCompleted} time`
+            : `Completed ${workout.TimesCompleted} times`}
+        </MG.WOTimesCompleted>
+        <MG.WOLastCompleted>{`Last completed on: ${workout.LastCompleted}`}</MG.WOLastCompleted>
+        <GS.Button onClick={routeChange}>START</GS.Button>
       </MG.WOItem>
       {showExercises && <ExercisePanel />}
     </MG.WOPanel>
