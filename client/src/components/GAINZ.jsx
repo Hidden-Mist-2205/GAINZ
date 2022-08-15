25f0624780e8b7ef5511bf788e51f1ab1c3554e6
Albert <albertthuynh94@gmail.com>
2022-08-15 10:39:33 -0400

    Updated Gainz

--- client/src/components/GAINZ.jsx
+++ client/src/components/GAINZ.jsx
@@ -1,30 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { LogoutButton } from './Userfront/Userfront';
 import NavBar from './NavBar';
 import GS from './styles/GeneralStyles';
-import Dashboard from './Dashboard/Dashboard';
-import FindWorkoutBuddy from './FindWorkoutBuddy/FindWorkoutBuddy';
-import MyGainz from './MyGainz/MyGainz';
-import MyProfile from './MyProfile/MyProfile';
-import StartWorkout from './StartWorkout/StartWorkout';
-import UserLibrary from './UserLibrary/UserLibrary';
+
+// import SamplePage from './Dashboard/SamplePage';
+// import FindWorkoutBuddy from './FindWorkoutBuddy/FindWorkoutBuddy';
+// import MyGainz from './MyGainz/MyGainz';
+// import MyProfile from './MyProfile/MyProfile';
+// import StartWorkout from './StartWorkout/StartWorkout';
+// import UserLibrary from './UserLibrary/UserLibrary';
+
+const SamplePage = lazy(() => import('./Dashboard/SamplePage'));
+const FindWorkoutBuddy = lazy(() => import('./FindWorkoutBuddy/FindWorkoutBuddy'));
+const MyGainz = lazy(() => import('./MyGainz/MyGainz'));
+const MyProfile = lazy(() => import('./MyProfile/MyProfile'));
+const StartWorkout = lazy(() => import('./StartWorkout/StartWorkout'));
+const UserLibrary = lazy(() => import('./UserLibrary/UserLibrary'));
 
 export default function GAINZ() {
   return (
     <GS.Background>
       <NavBar />
-      <Routes>
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/FindWorkoutBuddy" element={<FindWorkoutBuddy />} />
-        <Route path="/MyGainz" element={<MyGainz />} />
-        <Route path="/MyProfile" element={<MyProfile />} />
-        <Route path="/StartWorkout" element={<StartWorkout />} />
-        <Route path="/UserLibrary" element={<UserLibrary />} />
-      </Routes>
-      {/* <GS.Button>button</GS.Button>
+      <React.Suspense fallback={<span>Loading...</span>}>
+        <Routes>
+          <Route path="/Dashboard" element={<SamplePage />} />
+          <Route path="/FindWorkoutBuddy" element={<FindWorkoutBuddy />} />
+          <Route path="/MyGainz" element={<MyGainz />} />
+          <Route path="/MyProfile" element={<MyProfile />} />
+          <Route path="/StartWorkout" element={<StartWorkout />} />
+          <Route path="/UserLibrary" element={<UserLibrary />} />
+        </Routes>
+      </React.Suspense>
+      <GS.Button>button</GS.Button>
       <GS.OutlinedBtn> outlined </GS.OutlinedBtn>
-      <LogoutButton /> */}
+      <LogoutButton />
     </GS.Background>
   );
 }
