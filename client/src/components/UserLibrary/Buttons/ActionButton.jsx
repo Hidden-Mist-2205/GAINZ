218a762c8885c25564f1b5c06f2d8c074cb9c4a9
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-08-16 01:58:40 -0400

    modify ActionButton to accept clickHandler, add main Profile components

--- client/src/components/UserLibrary/Buttons/ActionButton.jsx
+++ client/src/components/UserLibrary/Buttons/ActionButton.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import GS from '../../styles/GeneralStyles';
 
-export default function ActionButton({ text }) {
+export default function ActionButton({ text, clickHandler }) {
   return (
-    <GS.Button>{text}</GS.Button>
+    <GS.Button onClick={clickHandler}>{text}</GS.Button>
   );
 }
