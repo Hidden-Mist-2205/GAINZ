8dc311ed4a1e859222588d490feca132ae6e96cb
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-08-18 12:16:59 -0400

    modify profile styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   rules: {
     'react/prop-types': 0,
     'arrow-parens': 0,
-    'jsx-a11y/label-has-associated-control': [2,{
+    'jsx-a11y/label-has-associated-control': [2, {
       assert: 'either',
     }],
   },
--- client/src/components/Profile/ProfileText.jsx
+++ client/src/components/Profile/ProfileText.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function ProfileText({ text }) {
   return (
-    <p>{text}</p>
+    <StyledProfileText>{text}</StyledProfileText>
   );
 }
+
+const StyledProfileText = styled.p`
+  margin: 0;
+  line-height: 1.8em;
+`;
