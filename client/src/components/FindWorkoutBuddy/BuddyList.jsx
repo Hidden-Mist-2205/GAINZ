e8c8042b9ee1c54562bc679aac4d42fed037b620
Patrick O'Shea <osheap81796@gmail.com>
2022-08-18 20:20:18 -0400

    Changed Find buddy styling

--- client/src/components/FindWorkoutBuddy/BuddyList.jsx
+++ client/src/components/FindWorkoutBuddy/BuddyList.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
+import { useAutoAnimate } from '@formkit/auto-animate/react'
+
 import BuddyListItem from './BuddyListItem';
 import PageListNav from '../UserLibrary/LibComponents/PageListNav';
 
-export default function BuddyList({ items, actionButton }) {
+export default function BuddyList({ items }) {
+  const [animationParent] = useAutoAnimate();
   return (
-    <>
-      {items.map(item => (
-        <BuddyListItem data={item} actionButton={actionButton} key={item.id} />
+    <div ref={animationParent}>
+      {items.map((item) => (
+        <BuddyListItem data={item} key={item.id} />
       ))}
       <PageListNav />
-    </>
+    </div>
   );
 }
--- test/backend/backend.test.js
+++ test/backend/backend.test.js
@@ -5,12 +5,12 @@ const controllers = require('../../database/controllers');
 request = request('http://localhost:3000/');
 
 describe('GET /products', () => {
-  it('should respond with status 200 and an array', async () => {
-    const userInfo = await controllers.getUserData(1);
-    //request
-    //  .get('/products')
-    //  .expect((res) => expect(Array.isArray(res.body)).toBeTruthy())
-    //  .expect(200, done);
-    expect(Array.isArray(userInfo)).toBeTruthy();
+  it('should respond with status 200 and an array', (done) => {
+    //const userInfo = await controllers.getUserData(1);
+    request
+      .get('/getUserInfo')
+      .expect((res) => expect(res.body).toBe("ok"))
+      .expect(200, done);
+    //expect(Array.isArray(userInfo)).toBeTruthy();
   });
 });
