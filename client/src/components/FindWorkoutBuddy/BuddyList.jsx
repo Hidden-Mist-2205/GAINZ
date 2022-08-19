59fb6625d1c72a887819583e4b8b46fa4335fa63
Patrick O'Shea <osheap81796@gmail.com>
2022-08-19 10:27:55 -0400

    Styling

--- client/src/components/FindWorkoutBuddy/BuddyList.jsx
+++ client/src/components/FindWorkoutBuddy/BuddyList.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-
 import { useAutoAnimate } from '@formkit/auto-animate/react'
-
 import BuddyListItem from './BuddyListItem';
-import PageListNav from '../UserLibrary/LibComponents/PageListNav';
 
 export default function BuddyList({ items }) {
   const [animationParent] = useAutoAnimate();
